6150288b783a7712842f9e7ec75efcca6ca20327
dslucasribeiro <dslucasribeiro@gmail.com>
2024-02-07 21:02:07 -0300

    fix: add requirements para deploy e corrigido backend

--- src/app.py
+++ src/app.py
@@ -8,8 +8,8 @@ def main():
     upload_file = ui.upload_file()
 
     if upload_file:
-        result, erros = process_excel(upload_file)
-        ui.display_results(result, erros)
+        result, error = process_excel(upload_file)
+        ui.display_results(result, error)
 
 if __name__ == "__main__":
     main()
